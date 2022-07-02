a9835da1c3cd6485a4537a1c8abe30b1685a4149
Maiadaa <maiadahkhaled@gmail.com>
2022-07-02 20:27:58 +0200

    Update AWSdeepracer-CustomRewardFunc.py

--- AWSdeepracer-CustomRewardFunc.py
+++ AWSdeepracer-CustomRewardFunc.py
@@ -3,6 +3,9 @@ def reward_function(params):
     # Method: Follow center line #
     #                            #
 
+    # Initialize the reward with typical value 
+    reward = 1
+
     """ Reward when getting close to centerline """
     #   Step 1: Read input parameters
     track_width = params['track_width']
@@ -13,13 +16,13 @@ def reward_function(params):
     marker_3 = 0.5 * track_width
     #   Step 3: Give high reward when close to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1
+        reward += 1
     elif distance_from_center <= marker_2:
-        reward = 0.5
+        reward += 0.5
     elif distance_from_center <= marker_3:
-        reward = 0.1
+        reward += 0.1
     else: # likely crashed / about to off track
-        reward = 1e-3  
+        reward += 1e-3  
     
     """ Reward when on track & speedy
         Penalize when going off track or when moving slowly """
@@ -30,11 +33,11 @@ def reward_function(params):
     SPEED_THRESHOLD = 1.0 
     # Step 3: Assign rewards and penalties 
     if not all_wheels_on_track:
-        reward = 1e-3
+        reward += 1e-3
     elif speed < SPEED_THRESHOLD:
-        reward = 0.5
+        reward += 0.5
     else:
-        reward = 1.0
+        reward += 1.0
 
     """ Penalize on excessive steering """    
     # Step 1: Read input variable
